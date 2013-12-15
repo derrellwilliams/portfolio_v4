0bcac5aa35471f81dab984efa11ac9728f00e329
derrellwilliams <derrellwilliams@gmail.com>
2013-12-14 19:08:40 -0500

    Updates to hp

--- source/javascripts/main.js
+++ source/javascripts/main.js
@@ -754,32 +754,7 @@ if (Prism.languages.markup) {
     }
   };
 
-  window.HeadroomExample = HeadroomExample;
-  
-}());
-;(function() {
-  var styles = {
-    swing : {
-      initial : 'animated',
-      pinned : 'swingInX',
-      unpinned : 'swingOutX'
-    },
-    slide : {
-      initial : 'animated',
-      pinned : 'slideDown',
-      unpinned : 'slideUp'
-    },
-    flip : {
-      initial : 'animated',
-      pinned : 'flipInX',
-      unpinned : 'flipOutX'
-    },
-    bounce : {
-      initial : 'animated',
-      pinned : 'bounceInDown',
-      unpinned : 'bounceOutUp'
-    }
-  };
+
 
   var inputs = document.querySelector('form');
   var codeGenerator = new CodeGenerator(
