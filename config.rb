a490062b3c48e3720e8f6f029ad7b514dcc712bd
derrellwilliams <derrellwilliams@gmail.com>
2014-01-19 20:35:53 -0500

    running all images through image optim

--- config.rb
+++ config.rb
@@ -40,8 +40,6 @@ page "/contact.html", :layout => "contact"
 # Automatic image dimensions on image_tag helper
 # activate :automatic_image_sizes
 
-# Reload the browser automatically whenever files change
-activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -61,14 +59,13 @@ configure :build do
   # For example, change the Compass output style for deployment
   # activate :minify_css
 
-  # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
 
   # Enable cache buster
   # activate :asset_hash
 
-  # Use relative URLs
-  # activate :relative_assets
+ 
+  activate :relative_assets
 
   # Or use a different image path
   # set :http_prefix, "/Content/images/"
